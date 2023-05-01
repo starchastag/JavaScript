4270a6551baf408961b1a5bcca70228ee3c5163c
shubham kumar <90451740+starchastag@users.noreply.github.com>
2023-05-02 00:27:12 +0530

    today basic about javascript
    here you can see short notes about javascript

---
+++ basic/tutorial 1.js
@@ -0,0 +1,36 @@
+/* 
+-------------  Java Script introduction -------------------------
+
+
+1. JAva script can change HTML content --
+                 
+             getElementById()
+
+This method can change the element content (inner html ) 
+
+2. Java script can change HTMl Attribute values--
+
+                   
+
+
+
+
+------------------------------------------------------------------
+
+The <script> Tag in html , javascript code is inseted between <script> and </script> tags.
+ex.
+          <script>
+document.getElementById("demo").innerHTML = "My First JavaScript";
+        </script>
+
+
+
+JavaScript Functions and Events--
+
+
+         A JavaScript function is a block of JavaScript code, that can be executed when "called" for.
+
+For example,
+        a function can be called when an event occurs, like when the user clicks a button.
+
+  */
