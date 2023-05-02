1869eabd0f2b6ad7ddbe853fa95ccb0b75c28af4
shubham kumar <90451740+starchastag@users.noreply.github.com>
2023-05-03 00:36:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+// 1.ways to print in javascript
+// console.log("Hello word ");
+alert("hey i am sparta");
+document.write("this is document write ");
+
+
+// 2.javascript console API
+console.log("Hello world");
+console.assert(4 == 4);
+console.warn("this is warning");
+console.error("this is error");
+
+// 3. variable
+// variable = "Container to store the data value"
+var number1 = 34;
+var number2 = 54;
+console.log(number1 + number2);
+
+
+// data types in javascript---
+
+/*   There are two type data type in javascript
+      1.Primitive data type
+      2.Reference data type   
+*/
+// 1 .Primitive Data Type
+//Number
+var num1 = 455;
+var num2 = 564;
+
+
+// String
+var str1 = "This is a string";
+var str2 = 'This is also a string';
+
+// Object
+
+var marks = {
+    ravi: 99,
+    shubham: 98,
+    ashok: 78
+}
+//console.log(marks);
+// Booleans
+
+var a = true;
+var b = false;
+//console.log(a,b);
+
+// 2. Reference Data Type --
+var arr = [1,3,'Starc',4,5]
+//console.log(arr);
+
+// Operators in javascript
+
+var c = 45;
+var d = 56;
+console.log("This is value of c + d is ", c + d);
+console.log("This is value of c - d is ", c - d);
+console.log("This is value of c / d is ", c / d);
+console.log("This is value of c * d is ", c * d);
+console.log("This is value of c % d is ", c % d);
+
+
+
+
+
